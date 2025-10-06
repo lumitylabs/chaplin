e46d164f6c200418ed2dd9135dfd95baae7c6a2d
Rafael Souza <rafaelszcardoso@gmail.com>
2025-10-06 10:34:25 -0300

    Update route to render Login component as default

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
